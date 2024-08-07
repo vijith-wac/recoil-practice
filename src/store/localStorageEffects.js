6a9a054b21da08d114867a0cd8d22cb927260bf1
vijith-wac <vijith.p@webandcrafts.com>
2024-08-07 11:07:42 +0530

    localstorage arrow function shorthand

--- src/store/localStorageEffects.js
+++ src/store/localStorageEffects.js
@@ -1,14 +1,33 @@
 import { DefaultValue } from "recoil"
 
-export const localStorageEffect = function(key){
-    return function({setSelf,onSet}){
-        //initilaize the atom's from local storage
+// export const localStorageEffect = function(key){
+//     return function({setSelf,onSet}){
+//         //initilaize the atom's from local storage
+//         const savedValue = localStorage.getItem(key)
+//         if(savedValue!=null){
+//             setSelf(JSON.parse(savedValue))
+//         }
+
+//         //save the atom's change into localstorage
+//         onSet(newValue=>{
+//             if(newValue instanceof DefaultValue){
+//                 localStorage.removeItem(key)
+//             }else{
+//                 localStorage.setItem(key,JSON.stringify(newValue))
+//             }
+//         })
+//     }
+
+
+// }
+
+export const localStorageEffect = key =>({setSelf,onSet})=>{
+  //initilaize the atom's from local storage
         const savedValue = localStorage.getItem(key)
         if(savedValue!=null){
             setSelf(JSON.parse(savedValue))
         }
-
-        //save the atom's change into localstorage
+         //save the atom's change into localstorage
         onSet(newValue=>{
             if(newValue instanceof DefaultValue){
                 localStorage.removeItem(key)
@@ -16,8 +35,6 @@ export const localStorageEffect = function(key){
                 localStorage.setItem(key,JSON.stringify(newValue))
             }
         })
-    }
-
 
 }
 
